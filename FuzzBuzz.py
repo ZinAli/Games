6b331c1c5071a3ec136d16e1b3490b9e313ee24e
ZinAli <33606085+ZinAli@users.noreply.github.com>
2024-09-15 01:19:02 +0300

    Update FuzzBuzz.py

--- FuzzBuzz.py
+++ FuzzBuzz.py
@@ -1,9 +1,18 @@
+
+# Create a loop with For and Range to go from 1 to 100.
 for number in range(1, 101):
-    if number % 3 != 0 and number % 5 != 0:
-        print(number)
-    elif number % 3==0:
-        print("Fizz")
-    elif number % 5==0:
-        print("Buzz")
-    elif number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
+  # First check if the number is divisible by both 3 and 5.
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+ 
+  # Then check if the number is only divisible by 3
+  elif number % 3 == 0:
+    print("Fizz")
+ 
+  # Finally check if the number is only divisible by 5
+  elif number % 5 == 0:
+    print("Buzz")
+ 
+  # If it's not divisible by either of those numbers, just print the number
+  else:
+    print(number)
